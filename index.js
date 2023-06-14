45a23ac366f5448b847ff4b95088c06ad7a64cd3
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2023-06-14 16:10:10 -0700

    Updated logging and error handling

--- index.js
+++ index.js
@@ -3,79 +3,99 @@ import axios from "axios";
 import puppeteer from "puppeteer";
 import { program } from "commander";
 
+// Define Options
+program
+  .requiredOption(
+    "-u, --url <string>",
+    "The base URL to use for crawl (required)"
+  )
+  .option("-s, --select <string>", "CSS selector for images", "img")
+  .option("-n, --next <string>", "CSS selector for link to next page")
+  .option("-l, --limit <int>", "Maximum number of pages to crawl", 10)
+  .option("-v, --verbose")
+  .parse();
+
+// Get Options
+const { url: baseUrl, select, next, limit, verbose } = program.opts();
+
+// Verbose Logging
+if (verbose) console.log("Verbose logging enabled");
+
 // Download Function
 async function download(url, filepath) {
-  console.log("Downloading img:", url);
+  try {
+    // Verbose Logging
+    if (verbose) console.log("Downloading img:", url);
 
-  const response = await axios({
-    url,
-    method: "GET",
-    responseType: "stream",
-  });
-  return new Promise((resolve, reject) => {
-    response.data
-      .pipe(fs.createWriteStream(filepath))
-      .on("error", reject)
-      .once("close", () => resolve(filepath));
-  });
+    // Fetch asset
+    const response = await axios({
+      url,
+      method: "GET",
+      responseType: "stream",
+    });
+    await new Promise((resolve, reject) => {
+      response.data
+        .pipe(fs.createWriteStream(filepath))
+        .on("error", reject)
+        .once("close", () => resolve(filepath));
+    });
+  } catch (error) {
+    console.error("Error downloading img:", url);
+    console.error(error);
+  }
 }
 
 (async () => {
-  // Define Options
-  program
-    .requiredOption(
-      "-u, --url <string>",
-      "The base URL to use for crawl (required)"
-    )
-    .option("-s, --select <string>", "CSS selector for images", "img")
-    .option("-n, --next <string>", "CSS selector for link to next page")
-    .option("-l, --limit <int>", "Maximum number of pages to crawl", 10)
-    .parse();
-
-  // Get Options
-  const { url: baseUrl, select, next, limit } = program.opts();
-
   // Set Current Page Index = 0
   let index = 0;
 
   // Scrape Page Function
   async function scrapePage(url, selector, page) {
-    // Load page
-    await page.goto(url);
-    console.log("Starting on:", url);
+    try {
+      // Load page
+      await page.goto(url);
 
-    // Wait for selector
-    await page.waitForSelector(selector);
+      // Basic Logging
+      console.log("Starting on:", url);
 
-    // Get the image URLs
-    const imageUrls = await page.evaluate((selector) => {
-      const images = Array.from(document.querySelectorAll(selector));
-      return images.map((image) => image.src);
-    }, selector);
+      // Wait for selector
+      await page.waitForSelector(selector);
 
-    console.log(`Found ${imageUrls.length} images`);
+      // Get the image URLs
+      const imageUrls = await page.evaluate((selector) => {
+        const images = Array.from(document.querySelectorAll(selector));
+        return images.map((image) => image.src);
+      }, selector);
 
-    // Download images
-    for (const imageUrl of imageUrls) {
-      const filename = imageUrl.split("/").pop();
-      const localPath = `./images/${filename}`;
+      // Basic Logging
+      console.log(`Found ${imageUrls.length} images`);
 
-      // Download image
-      await download(imageUrl, localPath);
-    }
+      // Download images in parallel
+      await Promise.all(
+        imageUrls.map((imageUrl) => {
+          const filename = imageUrl.split("/").pop();
+          const localPath = `./images/${filename}`;
+          return download(imageUrl, localPath);
+        })
+      );
 
-    console.log("Completed page");
+      // Basic Logging
+      console.log(`Completed page ${index}`);
 
-    // Crawl?
-    if (next && index < limit) {
-      // Increment index
-      index++;
-      // Get URL
-      const nextUrl = await page.evaluate((next) => {
-        return document.querySelector(next).href;
-      }, next);
-      // Scrape Next Page
-      await scrapePage(nextUrl, select, page);
+      // Crawl?
+      if (next && index < limit) {
+        // Increment index
+        index++;
+        // Get URL
+        const nextUrl = await page.evaluate((next) => {
+          return document.querySelector(next).href;
+        }, next);
+        // Scrape Next Page
+        await scrapePage(nextUrl, select, page);
+      }
+    } catch (error) {
+      console.error("Error scraping page:", url);
+      console.error(error);
     }
   }
 
@@ -83,11 +103,16 @@ async function download(url, filepath) {
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
-  // Scrape Base Page
-  await scrapePage(baseUrl, select, page);
-
-  // Done
-  console.log("Crawl complete, exiting.");
-  await browser.close();
-  process.exit();
+  try {
+    // Scrape Base Page
+    await scrapePage(baseUrl, select, page);
+  } catch (error) {
+    console.error("Error during scraping:");
+    console.error(error);
+  } finally {
+    // Done
+    console.log("Crawl complete, exiting.");
+    await browser.close();
+    process.exit();
+  }
 })();
